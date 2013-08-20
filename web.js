222f5b134615ca528899a829fd18bc87fac8eb6d
npeprah <nanakwesi.peprah@gmail.com>
2013-08-19 21:57:44 -0400

    Completed part one of homework

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+function readHtml(index) {
+	return fs.readFileSync(index,'utf-8');
+}
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readHtml('index.html'));
 });
 
 var port = process.env.PORT || 5000;
